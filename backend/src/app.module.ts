20974503bc925102bb974dfc831654f5b4002804
Nattapong <banker.z412@gmail.com>
2023-09-01 17:22:20 +0700

    set up nestjs before connect knexjs

---
+++ backend/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TicketsModule } from './tickets/tickets.module';
+
+@Module({
+  imports: [UsersModule, TicketsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
