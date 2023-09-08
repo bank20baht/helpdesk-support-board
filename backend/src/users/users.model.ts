6b5fde81f0ae5a5a21b24df7c09e283fa8ece37f
Nattapong <banker.z412@gmail.com>
2023-09-08 13:56:40 +0700

    beforeInsert in objection it working

--- backend/src/users/users.model.ts
+++ backend/src/users/users.model.ts
@@ -1,5 +1,7 @@
 import { Model } from 'objection';
+import * as bcrypt from 'bcrypt';
 import Tickets from 'src/tickets/tickets.model';
+
 export default class Users extends Model {
   static get tableName() {
     return 'users';
@@ -18,6 +20,7 @@ export default class Users extends Model {
       },
     };
   }
+
   id: number;
   name: string;
   email: string;
@@ -26,4 +29,22 @@ export default class Users extends Model {
   role: string;
   created_at: Date;
   updated_at: Date;
+
+  async $beforeInsert(queryContext: any) {
+    // Call the parent class's $beforeInsert method first
+    await super.$beforeInsert(queryContext);
+
+    // Make sure 'this.password' is set properly
+    if (this.password) {
+      // Hash the password before inserting it into the database
+      const hashedPassword = await bcrypt.hash(this.password, 10);
+      this.password = hashedPassword;
+    }
+
+    // Add any additional actions you want to perform before inserting here
+    // For example, you can add a timestamp for created_at and updated_at fields
+    const currentDate = new Date();
+    this.created_at = currentDate;
+    this.updated_at = currentDate;
+  }
 }
