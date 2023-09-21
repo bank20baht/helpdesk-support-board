445e211b031b6efa4d37e3e3ffe3c4d62e481ea3
Nattapong <banker.z412@gmail.com>
2023-09-21 17:44:08 +0700

    add swagger to create api docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('help desk support')
+    .setDescription('help desk API Document')
+    .setVersion('1.0')
+    .addTag('helpdesk')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(5000);
 }
 bootstrap();
